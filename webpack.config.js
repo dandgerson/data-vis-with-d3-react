700c726fe95b51a3d5db7182f934eea8b12f6e5a
Dmitry G. Anderson <dandgerson@gmail.com>
2021-07-04 02:17:39 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+const path = require('path')
+const { merge } = require('webpack-merge')
+const parts = require('./webpack.parts')
+
+const commonConfig = merge([
+  {
+    entry: ['./src'],
+    output: {
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+      modules: [path.resolve('./src'), path.resolve('./node_modules')],
+    },
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+    },
+  },
+  parts.page({
+    title: 'Data Visualization with D3 and React',
+    template: path.resolve(__dirname, '/public/index.html'),
+  }),
+  parts.loadJs(),
+  parts.extractCss({
+    loaders: [parts.autoprefix(), parts.sassLoader()],
+  }),
+  parts.extractCssModules({
+    loaders: [parts.autoprefix(), parts.sassLoader()],
+  }),
+  parts.loadImages({ limit: 500 }),
+  parts.loadFonts(),
+])
+
+const productionConfig = merge([parts.eliminateUnusedCss()])
+
+const developmentConfig = merge([
+  {
+    output: {
+      assetModuleFilename: '[name][ext][query]',
+    },
+    devtool: 'source-map',
+  },
+])
+
+/**
+ * Alternate way to get the mode value
+ *
+ * const mode = process.argv[process.argv.indexOf('--mode') + 1]
+ */
+const getConfig = (mode) => {
+  switch (mode) {
+    case 'production':
+      return merge(commonConfig, productionConfig, { mode })
+    case 'development':
+      return merge(commonConfig, developmentConfig, { mode })
+    default:
+      throw new Error(`Trying to use an unknow mode, ${mode}`)
+  }
+}
+
+module.exports = (env, options) => {
+  console.log({ env, options })
+  return getConfig(options.mode)
+}
---
+++ webpack.parts.js
@@ -0,0 +1,144 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+exports.page = ({ title, template }) => ({
+  plugins: [
+    new HtmlWebpackPlugin({
+      title,
+      template,
+    }),
+  ],
+})
+
+exports.loadCss = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+})
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+exports.extractCss = ({ options = {}, loaders = [] } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [{ loader: MiniCssExtractPlugin.loader, options }, { loader: 'css-loader' }].concat(loaders),
+        exclude: /\.m\.(sa|sc|c)ss$/i,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+})
+
+exports.extractCssModules = ({ options = {}, loaders = [] } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.m\.(sa|sc|c)ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options },
+          { loader: 'css-modules-typescript-loader?modules' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]_[local]--[hash:base64:3]',
+                // auto: /\.m\.\w+$/i,
+              },
+            },
+          },
+        ].concat(loaders),
+        sideEffects: true,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.WatchIgnorePlugin({
+      paths: [/\.m\.(sa|sc|c)ss\.d\.ts$/],
+    }),
+  ],
+})
+
+exports.sassLoader = (options = {}) => ({
+  loader: 'sass-loader',
+  options,
+})
+
+const glob = require('glob')
+const PurgeCssWebpackPlugin = require('purgecss-webpack-plugin')
+
+const allFiles = glob.sync(path.join(__dirname, 'src/*.js'))
+
+exports.eliminateUnusedCss = () => ({
+  plugins: [
+    new PurgeCssWebpackPlugin({
+      paths: allFiles,
+      extractors: [
+        {
+          extractor: (content) => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ['html'],
+        },
+      ],
+    }),
+  ],
+})
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    postcssOptions: { plugins: [require('autoprefixer')()] },
+  },
+})
+
+exports.loadImages = ({ limit }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|svg)$/,
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: limit } },
+      },
+    ],
+  },
+})
+
+exports.loadFonts = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+})
+
+exports.loadJs = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, './src'),
+        exclude: [/node_modules/],
+        use: 'babel-loader',
+      },
+    ],
+  },
+})
