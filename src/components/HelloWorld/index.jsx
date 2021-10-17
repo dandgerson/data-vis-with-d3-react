2d7b470c1c0b21d52643f1a8aa5cde061dbc681a
Dmitry G. Anderson <dandgerson@gmail.com>
2021-10-17 12:50:22 +0300

    drop unused

--- src/components/HelloWorld/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import cl from 'clsx'
-
-import s from './HelloWorld.m.scss'
-
-const HelloWorld = () => <div className={cl(s.root)}>Hello Flat World!!!</div>
-
-export default HelloWorld
