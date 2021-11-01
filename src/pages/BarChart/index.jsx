990444654d05558c4009edad343e25afe9aa0c17
Dmitry G. Anderson <dandgerson@gmail.com>
2021-11-01 07:57:10 +0300

    add axis

--- src/pages/BarChart/index.jsx
+++ src/pages/BarChart/index.jsx
@@ -19,32 +19,34 @@ const BarChart = () => {
 
   const processedData = useMemo(() => data.slice(0, 10), [data])
 
-  const svgContainerProps = useMemo(() => {
-    const svgRect = document.querySelector('[data-svg-container]')?.getBoundingClientRect()
-
-    return {
-      width: svgRect?.width || 0,
-      height: svgRect?.height || 0,
-    }
-  }, [processedData])
-
   const c = {
     margin: {
       top: 20,
       right: 20,
       bottom: 20,
-      left: 20,
+      left: 200,
     },
   }
+
+  const svgContainerProps = useMemo(() => {
+    const svgRect = document.querySelector('[data-svg-container]')?.getBoundingClientRect()
+    const width = svgRect?.width || 0
+    const height = svgRect?.height || 0
+
+    return {
+      innerWidth: width - (c.margin.left + c.margin.right),
+      innerHeight: height - (c.margin.top + c.margin.bottom),
+    }
+  }, [processedData])
 
   const d3Props = useMemo(
     () => ({
       yScale: scaleBand()
         .domain(processedData.map(d => d.Country))
-        .range([0, svgContainerProps.height - (c.margin.top + c.margin.bottom)]),
+        .range([0, svgContainerProps.innerHeight]),
       xScale: scaleLinear()
         .domain([0, Math.max(...processedData.map(d => d.population))])
-        .range([0, svgContainerProps.width - (c.margin.left + c.margin.right)]),
+        .range([0, svgContainerProps.innerWidth]),
     }),
     [processedData],
   )
@@ -83,6 +85,36 @@ const BarChart = () => {
           }}
         >
           <g transform={`translate(${c.margin.left},${c.margin.top})`}>
+            {d3Props.xScale.ticks().map(tickValue => (
+              <g key={tickValue} transform={`translate(${d3Props.xScale(tickValue)},0)`}>
+                <line y2={svgContainerProps.innerHeight} stroke='white' />
+                <text
+                  style={{
+                    textAnchor: 'middle',
+                    fill: 'white',
+                  }}
+                  dy='.71em'
+                  y={svgContainerProps.innerHeight + 3}
+                >
+                  {tickValue}
+                </text>
+              </g>
+            ))}
+
+            {d3Props.yScale.domain().map(countryName => (
+              <text
+                style={{
+                  textAnchor: 'end',
+                  fill: 'white',
+                }}
+                dy='.32em'
+                y={d3Props.yScale(countryName) + d3Props.yScale.bandwidth() / 2}
+                x={-3}
+              >
+                {countryName}
+              </text>
+            ))}
+
             {processedData.map(d => (
               <rect
                 key={d.Country}
