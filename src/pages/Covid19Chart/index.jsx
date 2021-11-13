93f78036eb6817b1e2a50aa492aa81995a8e66cc
Dmitry G. Anderson <dandgerson@gmail.com>
2021-11-13 19:05:44 +0300

    add sidebar & get lastDateColumn

--- src/pages/Covid19Chart/index.jsx
+++ src/pages/Covid19Chart/index.jsx
@@ -1,5 +1,5 @@
+import React, { useMemo } from 'react'
 import useCsv from 'hooks/useCsv'
-import React from 'react'
 
 const Covid19Chart = () => {
   const [data] = useCsv(
@@ -8,7 +8,59 @@ const Covid19Chart = () => {
 
   console.log({ data })
 
-  return <div>Hello wolrd</div>
+  const processedData = useMemo(() => {
+    const lastDateColumn = data.columns[data.columns.length - 1]
+    return lastDateColumn
+  }, [data])
+
+  console.log({ processedData })
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+      }}
+    >
+      <div
+        style={{
+          width: '20%',
+        }}
+      >
+        <h2>Covid-19 Chart</h2>
+        <br />
+
+        <p>
+          This program loads and parses the
+          {' '}
+          <a
+            className='a'
+            target='_blank'
+            href='https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+            rel='noreferrer'
+          >
+            Jhons Hopkins Coronavirus Dataset
+          </a>
+        </p>
+      </div>
+
+      <div className='separator-v'>
+        <div />
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          flex: 1,
+        }}
+      >
+        <h2>Covid-19 Chart</h2>
+
+        <p>Hello wolrd</p>
+      </div>
+    </div>
+  )
 }
 
 export default Covid19Chart
